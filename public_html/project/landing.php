c137bb6032da24291abde6e65065dd5a89fdb544
npape11 <nmpape11@gmail.com>
2025-07-07 21:19:41 -0400

    added helper functions and templates

--- public_html/project/landing.php
+++ public_html/project/landing.php
@@ -4,12 +4,11 @@ require(__DIR__."/../../partials/nav.php");
 <h1>Landing Page</h1>
 <?php
 error_log("Session: ". var_export($_SESSION, true));
-if(isset($_SESSION["user"], $_SESSION["user"]["email"])){
- echo "Welcome, ";
+if(is_logged_in()){
+ echo "Welcome, . get_user_email()";
  // Always escape user data before outputting it to prevent XSS, even if you trust your session data
- se($_SESSION["user"]["email"]);
 }
-else{
+else {
   echo "You're not logged in";
 }
 ?>
